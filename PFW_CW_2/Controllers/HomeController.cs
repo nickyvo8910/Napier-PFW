d5ec0f0615ca4ad28a81a423946dc17afebdee89
Nicky Vo <nickyvo8910@gmail.com>
2020-04-16 02:07:22 +0100

    Working as CW1

--- PFW_CW_2/Controllers/HomeController.cs
+++ PFW_CW_2/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace PFW_CW_2.Controllers
@@ -10,21 +7,66 @@ namespace PFW_CW_2.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return new CausesController().Index();
         }
-
         public ActionResult About()
         {
+            var mvcName = typeof(Controller).Assembly.GetName();
+            var isMono = Type.GetType("Mono.Runtime") != null;
+
+            ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+            ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult MoreInfo(int causeID)
+        {
+            return View();
+        }
+
+        public ActionResult LogIn()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
+
+        public ActionResult SignUp()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult MyProfile()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult MyCauses()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult MyNewCause()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        public ActionResult ErrorPage()
+        {
+            return View();
+        }
+
     }
 }
